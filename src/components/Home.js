83d9d5a6e83bfe122fc5b07141ae3d9f142aea45
marcoslopez <marcoslopezsanchez@hotmail.com>
2022-05-14 15:11:13 -0500

    Modificando Home, tambien arreglando errores no graves

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import './Home.css';
-import logo from './../img/logo.jpg';
+import "./Home.css";
+import logo from "./../img/logo.jpg";
 
 function Home() {
   return (
-    <div className='home' id=''>
-        <div className='home__bg'>
-            <div className='header d__flex align__items__center pxy__30'>
-                <div className='logo'>
-                    <img src={logo} alt=''/>
-                </div> {/* Pauta */}
-                <div className="navigation pxy__30">
+    <div className="home" id="">
+      <div className="home__bg">
+        <div className="header d__flex align__items__center pxy__30">
+          <div className="logo">
+            <img src={logo} alt="" />
+          </div>
+          {/* Pauta */}
+          <div className="navigation pxy__30">
             <ul className="navbar d__flex">
               <a href="#Home">
                 <li className="nav__items mx__15">Home</li>
@@ -33,10 +34,20 @@ function Home() {
               </a>
             </ul>
           </div>
-            </div>
         </div>
+        <div className="container">
+          <div className="home__content">
+            <div className="home__meta">
+              <h1 className="home__text pz__10">WELCOME</h1>
+              <h2 className="home__text pz__10">HI, MY NAME IS MARCOS</h2>
+              <h3 className="home__text sweet px__10">Js Developer</h3>
+              <h4 className="home__text pz__10"> Based in México</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Home
+export default Home;
