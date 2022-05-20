b8eb0549bce374ff816de03348e42d9b5294bb9d
MARCOS <marcoslopezsanchez@hotmail.com>
2022-05-19 21:28:38 -0500

    Se modificó y agrego el componente about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Home from "./components/Home";
 import "./App.css";
+import About from "./components/About";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <About/>
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
 
-import ('./About.css');
+import "./About.css";
+import aboutImg from "../img/about.jpg";
 
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="about component__space" id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col__2">
+            <img src={aboutImg} alt="" className="about__img" />
+          </div>
+          <div className="col__2">
+            <h1 className="about__heading"> About Me</h1>
+            <div className="about__meta">
+              <p className="p__color">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered in some form, by injected humour,
+                or randomised words which dont look even slightly believable. If
+                you are going to use a passage of Lorem Ipsum,
+              </p>
+              <p className="p__color">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered in some form, by injected humour,
+                or randomised words which dont look even slightly believable. If
+                you are going to use a passage of Lorem Ipsum,
+              </p>
+              <p className=" p__color">
+                There are many variations of passages of Lorem Ipsum available,
+                but the majority have suffered in some form, by injected humour,
+                or randomised words which dont look even sligh
+              </p>
+              <div className="about__button d__flex align__items__center">
+                <a href="#">
+                  <button className="about btn pointer">Downloader CV</button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
