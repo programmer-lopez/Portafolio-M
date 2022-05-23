44ab3f58a84112a0635f800b80f38038b8df29d9
MARCOS <marcoslopezsanchez@hotmail.com>
2022-05-23 16:30:09 -0500

    Add service and complements heard

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import About from "./components/About";
 import Home from "./components/Home";
+import Service from "./components/Service";
 
 function App() {
   return (
     <div className="App">
       <Home/>
       <About/>
+      <Service/>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import "./Home.css";
-import logo from "./../img/logo.jpg";
+import logo from "./../img/Logo.png";
 
 function Home() {
   return (
